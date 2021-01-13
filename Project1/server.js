2f71550c32a6cae4b88d3b3b62d8bb607426d674
banu tryon <banutryon@banus-mbp.lan>
2021-01-13 14:30:26 -0800

    built templet server.js and index.html

--- Project1/server.js
+++ Project1/server.js
@@ -1,1 +1,44 @@
-console.log('Hello world');
+// DEPENDENCIES
+const express = require('express')
+const mongoose = require('mongoose')
+
+// CONFIGURATION
+const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
+
+// DATABASE
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
+
+// Define callback functions for various events
+mongoose.connection.on('error', err =>
+console.log(
+  err.message,
+    ' is Mongod not running?/Problem with Atlas Connection?'
+  )
+)
+mongoose.connection.on('connected', () =>
+  console.log('mongo connected: ', MONGODB_URI))
+mongoose.connection.on('disconnected', () =>
+console.log('mongo disconnected'))
+
+
+// MIDDLEWARE
+app.use(express.json())
+app.use(express.static('public'))
+
+
+// Controllers
+const movieController = require('./controllers/movie_controller.js')
+app.use('/moviepropstore', movieController)
+
+
+// Listener
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
