88d280b61d297aa53c186595691a2aa86b91008e
banu tryon <banutryon@banus-mbp.lan>
2021-01-13 16:39:24 -0800

    bug fix

--- Project1/models/movie_prop.js
+++ Project1/models/movie_prop.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 const movieSchema = new mongoose.Schema({
   name: String,
-  image: String,
+  image: { type: String, default: 'https://media4.giphy.com/media/dwB8GZLYfZPig/giphy.gif?cid=ecf05e470df99d1816c15f419369d90e8cd97415df8b4684&rid=giphy.gif'},
   movie: String,
-  price: String
+  price: String,
 })
 
 const Movie = mongoose.model('Movie', movieSchema)
